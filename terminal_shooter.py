fbf6c5e849e650e106b4bdabd0a2fe244a9f4e5e
radu-pistea <radu_pistea@yahoo.com>
2023-11-03 11:28:21 +0000

    fixed healing, added armor damage, added stealth

--- terminal_shooter.py
+++ terminal_shooter.py
@@ -1,15 +1,22 @@
 import random
 
 # Global variables
-
+stealth_count = 0
 enemy_count = 1
 attack_multiplier = 1
-gifts = {1: "Basic Healing", 2: "Basic Healing", 3: "Superior healing", 4: "Basic armour", 5: "Superior armour"}
+gifts = {
+    1: "Basic Healing",
+    2: "Basic Healing",
+    3: "Superior healing",
+    4: "Basic armour",
+    6: "Basic Healing",
+    7: "Basic Healing"
+}
 gift_equivalent = {"Basic Healing": 25, "Superior healing": 50, "Basic armour": 25, "Superior armour": 50}
 
 # Define gift method at end of round
 def gift():
-    rand_gift = random.randint(1, 10)
+    rand_gift = random.randint(1, 13)
     if rand_gift in gifts.keys():
         if gifts[rand_gift] in list(gift_equivalent.keys())[:2]:
             player.heal[len(player.heal)+1] = gifts[rand_gift]
@@ -23,6 +30,27 @@ def gift():
     else:
         print("Sorry! No gift for you! :(")
 
+def stealth():
+    global stealth_count
+    random_stealth = random.randint(1, 100)
+    if random_stealth <= Player.stealth:
+        print("Successful stealth! Enemy attack missed!")
+        stealth_count += 1
+        if stealth_count == 5:
+            Player.stealth += 5
+            stealth_count = 0
+        endround()
+    else:
+        print("You have been found!")
+        ai_turn()
+        endround()
+
+
+
+
+
+
+
 # Player class with stats
 class Player:
     hp = 100
@@ -30,7 +58,7 @@ class Player:
     armor = 0
     max_armor = 100
     weapon_level = 1
-    stealth = 0
+    stealth = 10
     max_stealth = 75
     kill_count = 0
     damage = 25
@@ -64,10 +92,11 @@ def calculate_enemy_damage(attack_dice, defender_dice):
     return damage
 
 def endround():
-    global enemy_count
+    global enemy_count, stealth_count
     if player.hp <= 0:
         print(f"You died! Kill Count: {player.kill_count}")
         enemy_count = 1
+        stealth_count = 0
         player.heal.clear()
     else:
         gift()
@@ -94,7 +123,15 @@ def ai_turn():
             break
         else:
             damage = attack_multiplier * calculate_enemy_damage(attack_dice, defender_dice)
-            player.hp -= damage
+            if player.armor > 0:
+                if player.armor - damage / 2 <= 0:
+                    damage -= player.armor * 2
+                    player.armor = 0
+                    player.hp -= damage
+                elif player.armor - damage / 2 > 0:
+                    player.armor -= int(damage / 2)
+            else:
+                player.hp -= damage
             print(f"\nAttack hit: {damage} dmg")
             attack_multiplier = 1
             break
@@ -117,7 +154,7 @@ while True:
             print("1. Attack")
             print("2. Heal")
             print("3. Hide")
-            print(f"HP: {player.hp}/{player.max_hp} | Armor: {player.armor}/{player.max_armor} | Weapon Level: {player.weapon_level} | Stealth: {player.stealth}% | Kill Count: {player.kill_count} | Enemy HP: {enemy.hp}/100")
+            print(f"HP: {player.hp}/{player.max_hp} | Armour: {player.armor}/{player.max_armor} | Weapon Level: {player.weapon_level} | Stealth: {player.stealth}% | Kill Count: {player.kill_count} | Enemy HP: {enemy.hp}/100")
             print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
             player_choice = input("\nEnter your choice: ")
             if player_choice == "1":
@@ -149,27 +186,31 @@ while True:
                         endround()
                         break
             elif player_choice == "2":
-                if len(player.heal) != 0:
+                if len(player.heal) == 0:
+                    print("Your inventory is empty!")
+                else:
                     while True:
                         print("\n-----------------")
                         print(f"Choose a healing potion:")
                         print(player.heal)
                         print("-----------------")
                         choice = input("\nEnter your choice: ")
-                        if choice not in player.heal:
-                            print("Invalid choice. Try again.")
-                        else:
+                        if int(choice) in player.heal:
                             player.hp += gift_equivalent[player.heal[int(choice)]]
                             player.heal.pop(int(choice))
                             break
+                        else:
+                            print("Invalid choice. Try again.")
                     if player.hp > 100:
                         player.hp = 100
                     ai_turn()
                     endround()
-                    break
-                else:
-                    print("Your inventory is empty!")
-                    break
+            elif player_choice == "3":
+                stealth()
+            elif player_choice.lower() == "quit":
+                break
+    elif choice.lower() == "quit":
+        break
     else:
         print("Invalid choice. Try again.")
 
